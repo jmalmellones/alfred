26e0a354d2347e51560c7272eccfc395a1c9b8f1
Jose Miguel Almellones Cabello <jmalmellones@gmail.com>
2016-04-18 22:07:45 +0200

    initial commit with pom and reports

---
+++ src/main/java/me/eightball/alfred/App.java
@@ -0,0 +1,13 @@
+package me.eightball.alfred;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
